0ff7f89a502ecbc4899ed37d121ef56953ca1265
Mohammad Fathizadeh <mohammad.fathizadeh@gmail.com>
2020-08-02 21:57:35 +0430

    implemented addTranslation method in Service Layer in TranslationService.java

---
+++ src/main/java/com/cubeia/exercise/exercise/service/TranslationService.java
@@ -0,0 +1,43 @@
+package com.cubeia.exercise.exercise.service;
+
+import com.cubeia.exercise.exercise.entity.Language;
+import com.cubeia.exercise.exercise.entity.Translation;
+import com.cubeia.exercise.exercise.repository.LanguageRepository;
+import com.cubeia.exercise.exercise.repository.TranslationRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+/**
+ * @author Mohammad Fathizadeh 2020-08-02
+ */
+@Service
+@Transactional
+public class TranslationService {
+
+    private final TranslationRepository translationRepository;
+    private final LanguageRepository languageRepository;
+
+    public TranslationService(TranslationRepository translationRepository, LanguageRepository languageRepository) {
+        this.translationRepository = translationRepository;
+        this.languageRepository = languageRepository;
+    }
+
+    public Translation addTranslation(com.cubeia.exercise.exercise.dto.Translation translation, String languageCode) {
+        if (translationRepository.findByKeyAndLanguageCode(translation.getKey(), languageCode) != null)
+            return null;
+
+        Translation newTranslation = new Translation();
+        newTranslation.setKey(translation.getKey());
+        newTranslation.setMeaning(translation.getMeaning());
+
+        Language language = languageRepository.findByCode(languageCode);
+
+        newTranslation.getLanguages().add(language);
+
+        return translationRepository.save(newTranslation);
+    }
+
+
+}
